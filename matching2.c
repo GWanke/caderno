974f5e657b6246a547bd11685ae2841e1cd0915e
Convidado <ra91671@uem.br>
2018-09-20 14:55:28 -0300

    Segundo ex

--- matching2.c
+++ matching2.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define LOW_VALUE "0"
 
-void inicializacao(FILE **arq1, FILE **arq2, char *prev1, char *prev2, int *flag) {
-	strcpy(prev1, LOW_VALUE);
-	strcpy(prev2, LOW_VALUE);
+void inicializacao(FILE **arq1, FILE **arq2, FILE **arq3, char *prev1, char *prev2, int *flag) {
+	strcpy(prev1, "0");
+	strcpy(prev2, "0");
 	*arq1 = fopen("arquivo1.txt", "r");
 	*arq2 = fopen("arquivo2.txt", "r");
 	if (*arq1 != NULL && *arq2 != NULL) {
 		*flag = 1;
 	}
-	FILE *arq3;
-	arq3 = fopen("arquivo3.txt", "w");
-	//return arq3;
+	*arq3 = fopen("arquivo3.txt", "w");
 }
 
 void input(FILE *arq, char *NOME_ANT, char *NOME, int *flag) {
-	if(fgets(NOME,50,arq)==NULL) {
+	fgets(NOME, 50, arq);
+	if (feof(arq)) {
 		*flag = 0;
-	} else {
-		NOME[strlen(NOME)-1] = '\0';
-	if (strcmp(NOME,NOME_ANT)>=0) {
+	} else if (strcmp(NOME_ANT,NOME)>=0) {
 		printf("Erro na checagem de sequencia\n");
 		exit(1);
-	}
 	} 
 	strcpy(NOME_ANT,NOME);
-	//return NOME;
 }
 
 void match() {
@@ -39,21 +33,17 @@ void match() {
 	char prev2[50];
 	char NOME1[50];
 	char NOME2[50];
-	printf("oba");
-	inicializacao(&arq1, &arq2, prev1, prev2, &flag);
-	printf("aqui");
+	inicializacao(&arq1, &arq2, &saida, prev1, prev2, &flag);
 	input(arq1, prev1, NOME1, &flag);
-	printf("buga");
 	input(arq2, prev2, NOME2, &flag);
-	printf("aba");
+	int comparacao = strcmp(NOME1,NOME2);
 	while (flag) {
-		if (strlen(NOME1) < strlen(NOME2)) {
+		if (comparacao < 0) {
 			input(arq1, prev1, NOME1, &flag);
-		} else if (strlen(NOME1) > strlen(NOME2)) {
+		} else if (comparacao > 0) {
 			input(arq2, prev2, NOME2, &flag);
 		} else {
 			fputs(NOME1, saida);
-			fputs("\n", saida);
 			input(arq1, prev1, NOME1, &flag);
 			input(arq2, prev2, NOME2, &flag);
 		}
